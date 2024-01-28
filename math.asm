bcbe612f22439e350d5dc9056c379b1e9633b83e
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2024-01-27 23:29:25 -0100

    Add files via upload

--- math.asm
+++ math.asm
@@ -1,8 +1,29 @@
 section .text
 
-global soma
+    global mais
+    global menos
+    global vezes
+    global dividir
 
-    soma:
-        mov EAX, EDI
-        add EAX, ESI
-        ret
+        mais:
+            mov EAX, EDI
+            add EAX, ESI
+            int 0x80
+            ret
+
+        menos:
+            mov EAX, EDI
+            sub EAX, ESI
+            ret
+
+        vezes:
+            mov EAX, EDI
+            imul EAX, ESI
+            ret
+
+        dividir:
+            mov EAX, EDI   
+            xor EDX, EDX   
+            div ESI         
+            ret                       
+    
